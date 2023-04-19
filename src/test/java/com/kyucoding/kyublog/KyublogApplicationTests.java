80cf075e9c3a29185725d3738e5af36753feb3a6
kyuwonhwang <kyuwon.hwang20@gmail.com>
2023-04-19 21:24:43 +0900

    initial commit

---
+++ src/test/java/com/kyucoding/kyublog/KyublogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyucoding.kyublog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyublogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
